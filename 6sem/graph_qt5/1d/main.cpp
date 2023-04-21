29e4df8eebb054e00a4cf10ac6910a3c26cb3f88
Nesterov-BA <nesterov.boris123@gmail.com>
2023-04-21 19:19:04 +0300

    qt5 initial
    added qt5 files from the vm

---
+++ 6sem/graph_qt5/1d/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "valInit.hpp"
+
+using namespace std;
+void initValues(double* Points, int size, int func, double* values);
+
+int main(int argc, char** argv){
+	double left, right, step;
+	int pointsCount, funcNum; 
+	
+	if (argc != 5){
+		cout << "INVALID NUMBER OF ARGUMENTS";
+		return -1;
+	}
+	
+	left = strtod(argv[1], NULL);
+	right = strtod(argv[2], NULL);
+	
+	if (!(right-left>0)){
+		cout << "INVALID INTERVAL BORDER";
+		return -2;
+	}
+	
+	pointsCount = atoi(argv[3]);
+	
+	if (pointsCount < 2) {
+		cout << "INVALID NUMBER OF INTERPOLATION POINTS";
+		return -3;		
+	}
+
+	funcNum = atoi(argv[4]);
+	step = (right-left)/(pointsCount-1);
+	
+	double* points = new double[pointsCount];
+	double* values = new double[pointsCount]; 
+	points[0] = left;
+	for (int i = 1; i < pointsCount; ++i) {
+		points[i] = points[i-1] + step; 
+	}
+
+	initValues(points, pointsCount, funcNum, values);
+
+	double* d = new double[pointsCount];
+	double* coefficents = new double[4*pointsCount];
+
+	for (int i = 1; i < pointsCount-1; i++)
+	{
+		d[i] = ((points[i+1] - points[i])*diffFun2(funcNum, points[i-1], points[i]) - (points[i] - points[i-1])*diffFun2(funcNum, points[i], points[i+1]))/(points[i+1]-points[i-1]);
+	}
+	d[0] = (3*diffFun2(funcNum, points[0], points[1]) - d2Fun(funcNum, points[0])*(points[1]-points[0])/2 - d[1])/2;
+	d[pointsCount-1] = (3*diffFun2(funcNum, points[pointsCount-2], points[pointsCount-1]) + d2Fun(funcNum, points[pointsCount-1])*(points[pointsCount-1]-points[pointsCount-2])/2 - d[pointsCount-2])/2;
+	// перекинуть вычисление d и коэффицентов в отдельный файл
+
+	
+	// for (int i = 0; i < pointsCount; i++)
+	// {
+	// 	cout << points[i] << " " << values[i] << "\n";
+	// }
+	
+	return 0;
+
+}
---
+++ 6sem/graph_qt5/1d/valInit.hpp
@@ -0,0 +1,96 @@
+#ifndef MYINIT
+#define MYINIT
+
+#include <cmath>
+using namespace std;
+
+double fun(int k, double x);
+double dFun(int k, double x);
+double diffFun2(int k, double x1, double x2);
+double diffFun3(int k, double x1, double x2, double x3);
+
+double fun(int k, double x)
+{
+	switch (k)
+	{
+	case 0:
+		return 1;
+		break;
+	case 1:
+		return x;
+		break;
+	case 2:
+		return pow(x, 2);
+		break;
+	case 3:
+		return pow(x, 3);
+		break;
+	case 4:
+		return pow(x, 4);
+		break;
+	case 5:
+		return exp(x);
+		break;
+	case 6:
+		return 1 / (25 * pow(x, 2) + 1);
+		break;
+	default:
+		cout << "INVALID FUNCTION NUMBER";
+		return -1;
+		break;
+	}
+}
+
+double d2Fun(int k, double x)
+{
+	switch (k)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 0;
+		break;
+	case 2:
+		return 2 ;
+		break;
+	case 3:
+		return 6*x;
+		break;
+	case 4:
+		return 12 * pow(x, 2);
+		break;
+	case 5:
+		return exp(x);
+		break;
+	case 6:
+		return (5000*pow(x,2))/pow(25*pow(x,2)+1, 3) - 50/pow(25*pow(x,2)+1, 2);
+		break;
+	default:
+		cout << "INVALID FUNCTION NUMBER";
+		return -1;
+		break;
+	}
+}
+
+double diffFun2(int k, double x1, double x2)
+{
+	double diff = (fun(k, x2) - fun(k, x1)) / (x2 - x1);
+	return diff;
+}
+
+double diffFun3(int k, double x1, double x2, double x3)
+{
+	double diff = (diffFun2(k, x2, x3) - diffFun2(k, x1, x2)) / (x3 - x1);
+	return diff;
+}
+
+void initValues(double *Points, int size, int func, double *values)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		values[i] = fun(func, Points[i]);
+	}
+}
+
+#endif
