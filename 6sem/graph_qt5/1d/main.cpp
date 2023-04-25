0223a5e58e52889df65090fed1d4c84e7c2a123e
Nesterov-BA <nesterov.boris123@gmail.com>
2023-04-25 10:07:41 +0300

    added coeff evaluation

--- 6sem/graph_qt5/1d/main.cpp
+++ 6sem/graph_qt5/1d/main.cpp
@@ -2,9 +2,12 @@ using namespace std;
 
 #include <iostream>
 #include "valInit.hpp"
+#include "coefficents.hpp"
 
 using namespace std;
 void initValues(double* Points, int size, int func, double* values);
+void coefficents_eval(double* Points, double* values, int size, int func, double* coeff);
+
 
 int main(int argc, char** argv){
 	double left, right, step;
@@ -42,17 +45,9 @@ int main(int argc, char** argv){
 
 	initValues(points, pointsCount, funcNum, values);
 
-	double* d = new double[pointsCount];
 	double* coefficents = new double[4*pointsCount];
-
-	for (int i = 1; i < pointsCount-1; i++)
-	{
-		d[i] = ((points[i+1] - points[i])*diffFun2(funcNum, points[i-1], points[i]) - (points[i] - points[i-1])*diffFun2(funcNum, points[i], points[i+1]))/(points[i+1]-points[i-1]);
-	}
-	d[0] = (3*diffFun2(funcNum, points[0], points[1]) - d2Fun(funcNum, points[0])*(points[1]-points[0])/2 - d[1])/2;
-	d[pointsCount-1] = (3*diffFun2(funcNum, points[pointsCount-2], points[pointsCount-1]) + d2Fun(funcNum, points[pointsCount-1])*(points[pointsCount-1]-points[pointsCount-2])/2 - d[pointsCount-2])/2;
-	// перекинуть вычисление d и коэффицентов в отдельный файл
-
+	
+	coefficents_eval(points, values, pointsCount, funcNum, coefficents);
 	
 	for (int i = 0; i < pointsCount; i++)
 	{
